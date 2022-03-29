5a892874a9bf6305bda800ac7024d40cf553866a
Hidayatullah Gustiawan Adi <adihidayatullah96.ah@gmail.com>
2022-03-29 19:36:20 +0700

    Upload file kalkulator
    calculator with minimum requirement

---
+++ script.js
@@ -0,0 +1,118 @@
+const number = document.querySelectorAll(".number")
+number.forEach((number)=>{
+    number.addEventListener("click",()=>{
+        console.log(event.target.value)
+
+    })
+})
+
+const calculatorScreen= document.querySelector('.calculator-screen')
+const updateScreen = (number) => {
+    calculatorScreen.value = number
+}
+
+const numbers = document.querySelectorAll(".number")
+numbers.forEach((number) => {
+    number.addEventListener("click", (event) => {
+        inputNumber(event.target.value)
+        // updateScreen(event.target.value)
+        updateScreen(currentNumber)
+    })
+})
+
+let prevNumber = ''
+let calculationOperator = ''
+let currentNumber = '0'
+
+const inputNumber = (number) => {
+    // currentNumber += number
+    if (currentNumber === '0') {
+        currentNumber = number
+    } else {
+        currentNumber += number
+    }
+}
+
+const operators = document.querySelectorAll(".operator")
+
+operators.forEach((operator)=>{
+    operator.addEventListener("click", (event) => {
+        // console.log(event.target.value)
+        inputOperator(event.target.value)
+    })
+})
+
+const inputOperator = (operator) => {
+    // prevNumber = currentNumber
+    // calculationOperator = operator
+    // currentNumber = ''
+    if (calculationOperator === '') {
+        prevNumber = currentNumber
+    }
+    calculationOperator = operator
+    currentNumber = '0'
+}
+
+const equalSign = document.querySelector('.equal-sign')
+
+equalSign.addEventListener('click',()=> {
+    // console.log('equal button is pressed')
+    calculate()
+    updateScreen(currentNumber)
+})
+
+const calculate= () => {
+    let result = ''
+    switch (calculationOperator) {
+        case '+':
+            result = parseFloat(prevNumber) + parseFloat(currentNumber)
+            break;
+        case '-':
+            result = parseFloat(prevNumber) - parseFloat(currentNumber)
+            break;
+        case '*':
+            result = parseFloat(prevNumber) * parseFloat(currentNumber)
+            break;
+        case '/':
+            result = parseFloat(prevNumber) / parseFloat(currentNumber)
+            break;
+    
+        default:
+            return;
+    }
+    currentNumber = result
+    calculationOperator = ''
+}
+
+const clearBtn = document.querySelector('.all-clear')
+
+clearBtn.addEventListener('click', () => {
+    // console.log('ac button is pressed')
+    clearAll()
+    updateScreen(currentNumber)
+})
+
+const clearAll = () => {
+    prevNumber = ''
+    calculationOperator = ''
+    currentNumber = '0'
+}
+
+const decimal = document.querySelector('.decimal')
+
+decimal.addEventListener('click', (event) => {
+    // console.log(event.target.value)
+    inputDecimal(event.target.value)
+    updateScreen(currentNumber)
+})
+
+inputDecimal = (dot) => {
+    // currentNumber += dot
+    if (currentNumber.includes('.')) {
+        return
+    }
+    currentNumber += dot
+}
+
+
+
